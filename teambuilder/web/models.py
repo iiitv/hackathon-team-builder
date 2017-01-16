290411640c64475ea3b02eb974f6e1436a15fbd5
Pratyush Singh <singh.pratyush96@gmail.com>
2017-01-16 14:46:40 +0530

    Defined models in web app. Closes #2

--- teambuilder/web/models.py
+++ teambuilder/web/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Participant(models.Model):
+
+    student_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=1024, default='', blank=False,
+                            null=False)
+    join_time = models.DateTimeField(default=timezone.now)
+
+
+class Skill(models.Model):
+
+    student = models.ForeignKey(Participant)
+    front_end = models.PositiveIntegerField(default=0, null=False)
+    back_end = models.PositiveIntegerField(default=0, null=False)
+    managing = models.PositiveIntegerField(default=0, null=False)
+    marketing = models.PositiveIntegerField(default=0, null=False)
+    # TODO: Verify
+
+
+class Team(models.Model):
+
+    name = models.CharField(primary_key=True, max_length=64)
+    create_time = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(Participant)
+
+
+class TeamMember(models.Model):
+
+    team = models.ForeignKey(Team)
+    member = models.ForeignKey(Participant, related_name='member')
+    add_time = models.DateTimeField(default=timezone.now)
+    added_by = models.ForeignKey(Participant, related_name='added_by')
+
+
+class TeamJoinRequest(models.Model):
+
+    team = models.ForeignKey(Team)
+    member = models.ForeignKey(Participant)
+    request_time = models.DateTimeField(default=timezone.now)
