f5835fae9ac208597e0d3233e55edaeb6dcca9f2
Pratyush Singh <singh.pratyush96@gmail.com>
2017-01-16 15:25:40 +0530

    Added basic home page

--- teambuilder/web/views.py
+++ teambuilder/web/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+from . import models
+
+
+def home(request):
+    user_id = request.COOKIES.get('userid', None)
+    user = models.Participant.objects.filter(student_id=user_id) if user_id \
+        else None
+    return render(request, 'home.html', context={
+        'title': 'Welcome to Hackathon 2017',
+        'user': user,
+    })
